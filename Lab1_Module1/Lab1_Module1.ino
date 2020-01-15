902fa244a2948c90e932efce7395809092715f01
Timothy Morland <thetimmorland@gmail.com>
2020-01-15 15:51:52 -0500

    update Lab1 Part1 code

--- Lab1_Module1/Lab1_Module1.ino
+++ Lab1_Module1/Lab1_Module1.ino
@@ -1,12 +1,12 @@
-
 #define LEDPIN 11
 #define BUTTON 10
 #define POT A0
+
 void setup() {
-  pinMode(BUTTON,INPUT);
-  pinMode(POT,INPUT);
-  pinMode(LEDPIN,OUTPUT):
-  Serial.begin(9600); //9600 bits per secondes 
+  pinMode(BUTTON, INPUT);
+  pinMode(POT, INPUT);
+  pinMode(LEDPIN, OUTPUT);
+  Serial.begin(9600); //9600 bits per secondes
 }
 
 int buttonIsPushed(int pin) {
@@ -14,19 +14,23 @@ int buttonIsPushed(int pin) {
 }
 
 void loop() {
+  int pot_val;
 
-  while(!buttonIsPushed(BUTTON)) {};
+  while (!buttonIsPushed(BUTTON)) {
+    Serial.println("waiting on button");
+  }
   delay(100);
-  while(buttonIsPushed(BUTTON)) {};
-  while(true) {
-    pot_val= analogRead(POT);
-    Serial.println(pot_val);
+
+  while (buttonIsPushed(BUTTON)) {
+    Serial.println("waiting for release");
+  }
+
+  while (true) {
+    pot_val = analogRead(POT);
+    Serial.println("blink");
     digitalWrite(LEDPIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(pot_val);               // wait for a second
     digitalWrite(LEDPIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(pot_val);  
-  } 
-  
-  
-
- 
+    delay(pot_val);
+  }
+}
