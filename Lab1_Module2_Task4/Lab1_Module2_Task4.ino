140f8d0831c27ec5ff4f4680389c65e44c00faaf
Timothy Morland <thetimmorland@gmail.com>
2020-01-15 17:29:25 -0500

    Finish lab 1

--- Lab1_Module2_Task4/Lab1_Module2_Task4.ino
+++ Lab1_Module2_Task4/Lab1_Module2_Task4.ino
@@ -5,46 +5,87 @@
 #define LED 3
 #define BUTTON 10
 
-struct motor {
-  int encoderPin;
-  int encoderState = 0;
-  int encoderCount = 0;
-  int rotationCount = 0;
-};
+volatile int leftEncoderCount = 0;
+volatile int rightEncoderCount = 0;
 
-motor m_left = {
-  .encoderPin = 3
-};
-
-motor m_right = {
-  .encoderPin = 2
-};
-
-void setup() {
-  pinMode(BUTTON, INPUT);
-  pinMode(POT, INPUT);
-  pinMode(LEDPIN, OUTPUT);
-  Serial.begin(9600); //9600 bits per secondes
-}
+void incLeftEncoderCount() { leftEncoderCount++; }
+void incRightEncoderCount() { rightEncoderCount++; }
 
 int buttonIsPushed(int pin) {
   return !digitalRead(pin);
 }
 
-int encoderChanged(int encoder) {
-    int currentState = digitalRead(encoder);
-    if (currentState != encoderState) {
-        encoderState = currentState;
-        return true;
-    }
+float rotationCount(int n) {
+  return (float) n / 16;
+}
 
-    return false;
+void setup() {
+  pinMode(SPEED_M1, OUTPUT);
+  pinMode(DIR_M1, OUTPUT);
+  pinMode(SPEED_M2, OUTPUT);
+  pinMode(DIR_M2, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  Serial.begin(9600); //9600 bits per secondes
+
+  while (!buttonIsPushed(BUTTON))
+  {
+    Serial.println("waiting on button");
+  }
+  delay(100);
+
+  while (buttonIsPushed(BUTTON))
+  {
+    Serial.println("waiting for release");
+  }
+
+  attachInterrupt(0, incLeftEncoderCount, CHANGE);
+  attachInterrupt(1, incRightEncoderCount, CHANGE);
+  interrupts();
+
+  // forward
+  while (true)
+  {
+    digitalWrite(DIR_M1, HIGH);
+    digitalWrite(DIR_M2, HIGH);
+    
+    analogWrite(SPEED_M1, 100);
+    analogWrite(SPEED_M2, 100);
+
+    if (rotationCount(leftEncoderCount) > 3) break;
+  }
+
+  leftEncoderCount = 0;
+  rightEncoderCount = 0;
+
+  // reverse
+  while (true) {
+    digitalWrite(DIR_M1, LOW);
+    digitalWrite(DIR_M2, LOW);
+
+    analogWrite(SPEED_M1, 100);
+    analogWrite(SPEED_M2, 100);
+
+    if (rotationCount(leftEncoderCount) > 2) break;
+  }
+  
+  leftEncoderCount = 0;
+  rightEncoderCount = 0;
+
+  // spin
+  while (true) {
+    digitalWrite(DIR_M1, HIGH);
+    digitalWrite(DIR_M2, LOW);
+
+    analogWrite(SPEED_M1, 100);
+    analogWrite(SPEED_M2, 100);
+
+    if (leftEncoderCount > 8) break;
+  }
+
+  analogWrite(SPEED_M1, 0);
+  analogWrite(SPEED_M2, 0);
 }
 
 void loop() {
-  if (encoderChanged()) {
-    encoderCount++;
-  }
-
-
 }
